0c354b1bfca01ccb19e3689d3998335a77a71509
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-05-29 15:18:09 +0200

    workshop solution

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
+      <HeroImage>
+        <source
+          type="image/avif"
+          srcSet="
+          /images/hero-img.avif 1x,
+          /images/hero-img@2x.avif 2x,
+          /images/hero-img@3x.avif 3x"
+        />
+        <source
+          type="image/jpeg"
+          srcSet="
+          /images/hero-img.jpg,
+          /images/hero-img@2x.jpg 2x,
+          /images/hero-img@3x.jpg 3x"
+        />
+        <img src="/images/hero-img.jpg" alt="Main site banner with a mysterious blue-eyed cat" />
+      </HeroImage>
       <Swoop src="/swoop.svg" />
     </Wrapper>
   );
@@ -20,7 +36,7 @@ const Wrapper = styled.section`
   background: hsl(0deg 0% 1%);
 `;
 
-const HeroImage = styled.img`
+const HeroImage = styled.picture`
   display: block;
   width: 500px;
   height: 500px;
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+  const sources = [
+    { type: "image/avif", ext: "avif", sizes: ["", "2x", "3x"] },
+    { type: "image/jpeg", ext: "jpg", sizes: ["", "2x", "3x"] },
+  ];
+
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          {sources.map(({ type, ext, sizes }) => (
+            <source
+              type={type}
+              srcSet={sizes.map((size) =>
+                src.replace(
+                  /\.\w+$/,
+                  `${size ? "@" + size : ""}.${ext} ${size}`
+                )
+              )}
+            />
+          ))}
+          <Image src={src} alt={alt} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,11 +46,13 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
+  white-space: nowrap;
   gap: 8px;
 `;
 
@@ -42,6 +62,12 @@ const Tag = styled.li`
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  &:last-of-type {
+    flex-shrink: 100;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 `;
 
 export default PhotoGridItem;
